a47cdeef88214c9b3af0cad3e73a82d74af71e6f
TorlettiJoaquin <Axeltorletti@gmail.com>
2023-06-07 19:19:44 -0300

    helatam v0.0.1

--- HELATAM/src/main.tsx
+++ HELATAM/src/main.tsx
@@ -2,10 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {  } from "module";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Home from './routes/home.tsx';
+import Guide from './routes/guide.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/guide",
+    element: <Guide />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
