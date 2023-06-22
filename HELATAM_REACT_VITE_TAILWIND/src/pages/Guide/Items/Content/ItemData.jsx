7b93ef0676e9ceeb3d894655e9036c5e348774b8
TorlettiJoaquin <Axeltorletti@gmail.com>
2023-06-22 00:33:52 -0300

    ITEM DATA actualizado

--- HELATAM_REACT_VITE_TAILWIND/src/pages/Guide/Items/Content/ItemData.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/pages/Guide/Items/Content/ItemData.jsx
@@ -1,24 +1,43 @@
-// const ItemData = [
-      
-//     {
-//         id: 10004,
-//         name: "ENCHANTED ORB",
-//         type: "3",
-//         img: "https://img6.99.com/yhkd/image/data/equip/12517.jpg",
-//         prop: "Health +300",
-//         effect: "Spell Resist +15",
-//         price: "2100",
-//         lvl: {
-//             id: 10004,
-//         },
-//     }
-// ]
+// const ITEM_IMG = "https://img6.99.com/yhkd/image/data/equip/"
+// const ITEM_TYPE = {
+//     "1" : "Fisico",
+//     "2" : "Magico",
+//     "3" : "Defensa",
+//     "4" : "Botas",
+//     "5" : "Accesorio",
+// }
 
-// export default ItemData
+const ItemData = [
+    {
+        id: 12517,
+        name: "RECOVERY RING",
+        type: "3",
+        img: "12517.jpg",
+        prop: ["Health Regen +6s per second"],
+        effect: [""],
+        price: 600,
+        lvl: 1,
+    },
+    {
+        id: 10107,
+        name: "PHANTOM CORSET",
+        type: "1",
+        img: "10107.jpg",
+        prop: ["Agility +6", "Attack Speed +10%"],
+        effect: ["Unique Passive: Recovers 5 Health with each attack"],
+        price: 600,
+        lvl: 1,
+    },
+    {
+        id: 10011,
+        name: "HASTE GLOVES",
+        type: "1",
+        img: "10011.jpg",
+        prop: ["Attack Speed +15"],
+        effect: [""],
+        price: 450,
+        lvl: 1,
+    },
+];
 
-var ItemData = {
-    "128001" : {},
-    "128002" : {},
-}
-
-export default ItemData
+export default ItemData;
