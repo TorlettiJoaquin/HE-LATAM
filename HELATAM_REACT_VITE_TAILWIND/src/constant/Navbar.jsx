60f5910591fd18bdb2181d1d551cb7f02642a3db
TorlettiJoaquin <Axeltorletti@gmail.com>
2023-06-25 23:25:09 -0300

    update header changes

--- HELATAM_REACT_VITE_TAILWIND/src/constant/Navbar.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/constant/Navbar.jsx
@@ -4,156 +4,156 @@ import { useState } from "react";
 // import logo from "../assets/images/newlogo.webp";
 // import hemlogo from "../assets/images/hemlogo.webp";
 import hemlogo2 from "../assets/images/hemlogo2.png";
-import { AiFillCaretDown }  from "react-icons/ai";
+
+import heico from "../../public/he.ico";
+import { AiFillCaretDown } from "react-icons/ai";
 
 export const Navbar = () => {
     const [openGf, setOpenGf] = useState(false);
     const [openRs, setOpenRs] = useState(false);
 
     return (
-        <nav className="relative w-3/4 max-w-[1700px] h-20 pl-[14%] bg-gradient-to-b from-blue-900 to-blue-950  mx-auto rounded-2xl z-50">
-            <div className="absolute -top-14 -left-12">
-                <Link to="/">
+        <div className="sticky top-0 grid grid-cols-[200px_1fr] gap-6 items-center bg-gradient-to-b from-blue-900 to-blue-950 shadow-[0_0_10px_0_rgba(0,0,0,.3)] z-20">
+            <div className="relative">
+                <Link
+                    to="/"
+                    className="block relative transform-none left-0 top-0 p-[5px_0_0_24px] max-w-none xl:pt-2 "
+                >
                     <img
                         src={hemlogo2}
                         alt="HEM MAWEL LOGO"
-                        className="w-[330px]"
+                        className="w-[200px] -mb-8 max-w-full"
                     />
-                </Link>
-                {/* <img src={logo} alt="HEM MAWEL LOGO" /> */}
+                </Link> 
             </div>
-            <ul className="flex justify-around text-base ml-20 h-full [&>*]:w-[14%] [&>*]:text-center [&>*]:leading-[70px] [&>*]:uppercase [&>*]:relative ">
-                <Link
-                    to="/"
-                    className="bg-gradient-to-b hover:from-blue-900 hover:to-slate-900 hover:border-b hover:border-b-yellow-200"
-                >
-                    <li>Inicio</li>
-                </Link>
-                <Link
-                    to="/guide/gameinfo"
-                    className="bg-gradient-to-b hover:from-blue-900 hover:to-slate-900 hover:border-b hover:border-b-yellow-200"
-                    onMouseEnter={() => {
-                        setOpenGf(true);
-                    }}
-                    onMouseLeave={() => {
-                        setOpenGf(false);
-                    }}
-                >
-                    <li>
-                        Informacion <AiFillCaretDown className="inline-block"/>
-                        
-                        <ul
-                            className={`${
-                                openGf ? "block" : "hidden"
-                            } absolute top-20 bg-slate-900 text-xs px-4 w-full [&>*]:[&>*]:h-14 [&>*]:[&>*]:leading-[55px] [&>*]:[&>*]:border-b [&>*]:[&>*]:border-b-blue-400 transition-all ease-linear duration-500`}
-                        >
-                            <Link to="/guide/whatishe">
-                                <li className="hover:border-b hover:border-b-yellow-200">
-                                    Juego
-                                </li>
-                            </Link>
-                            <Link to="/guide/terminology">
-                                <li className="hover:border-b hover:border-b-yellow-200">
-                                    Glosario
-                                </li>
-                            </Link>
-
-                            <Link to="/guide/battleground">
-                                <li className="hover:border-b hover:border-b-yellow-200">
-                                    batalla
-                                </li>
-                            </Link>
-
-                            <Link to="/guide/modes">
-                                <li className="hover:border-b hover:border-b-yellow-200">
-                                    Modos
-                                </li>
-                            </Link>
-
-                            <Link to="/guide/items">
-                                <li className="hover:border-b hover:border-b-yellow-200">
-                                    Items
-                                </li>
-                            </Link>
-                            <Link to="/media">
-                                <li className="hover:border-b hover:border-b-yellow-200">
-                                    Multimedia
-                                </li>
-                            </Link>
-                        </ul>
-                    </li>
-                </Link>
-                <Link
-                    to="/heroes"
-                    className="bg-gradient-to-b hover:from-blue-900 hover:to-slate-900 hover:border-b hover:border-b-yellow-200"
-                >
-                    <li className="">Heroes</li>
-                </Link>
-                <Link
-                    to="/news"
-                    className="bg-gradient-to-b hover:from-blue-900 hover:to-slate-900 hover:border-b hover:border-b-yellow-200"
-                >
-                    <li className="">Noticias</li>
-                </Link>
-                <Link
-                    to="/guide"
-                    className="bg-gradient-to-b hover:from-blue-900 hover:to-slate-900 hover:border-b hover:border-b-yellow-200"
-                >
-                    <li className="">Guia</li>
-                </Link>
-
-                <li
-                    className="bg-gradient-to-b hover:from-blue-900 hover:to-slate-900 hover:border-b hover:border-b-yellow-200 cursor-pointer"
-                    onMouseEnter={() => {
-                        setOpenRs(true);
-                    }}
-                    onMouseLeave={() => {
-                        setOpenRs(false);
-                    }}
-                >
-                    Comunidad <AiFillCaretDown className="inline-block"/>
-                    <ul
-                        className={`${
-                            openRs ? "block" : "hidden"
-                        } absolute top-20 bg-slate-900 text-xs px-4 w-full [&>*]:[&>*]:h-14 [&>*]:[&>*]:leading-[55px] [&>*]:[&>*]:border-b [&>*]:[&>*]:border-b-blue-400 transition-all ease-linear duration-500`}
-                    >
-                        <a
-                            href="https://www.facebook.com/hem.mawel"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <li className="hover:border-b hover:border-b-yellow-200">
-                                Facebook
+            <nav className="lg:block lg:static lg:h-auto lg:visible w-full">
+                <div className="grid grid-cols-[1fr_max-content] gap-2 h-auto max-w-none w-full justify-between pr-4">
+                    <ul className="flex items-center font-semibold [&>*]:mr-2 [&>*]:uppercase [&>*]:[&>*]:px-4 [&>*]:[&>*]:py-[7px] [&>*]:[&>*]: [&>*]:[&>*]:relative">
+                        <Link to="/" className="">
+                            <li className="active hover:active">
+                                Inicio
+                                <img src={heico} alt="" className="block absolute h-[16px] w-[16px] -bottom-2 left-[calc(50%_-_(16px/2))]" />
                             </li>
-                        </a>
-                        <a
-                            href="https://www.instagram.com/hemmawel/"
-                            target="_blank"
-                            rel="noreferrer"
+                        </Link>
+                        <Link
+                            to="/guide/gameinfo"
+                            className="hover:active"
+                            onMouseEnter={() => {
+                                setOpenGf(true);
+                            }}
+                            onMouseLeave={() => {
+                                setOpenGf(false);
+                            }}
                         >
-                            <li className="hover:border-b hover:border-b-yellow-200">
-                                Instagram
-                            </li>
-                        </a>
+                            <li>
+                                Informacion{" "}
+                                <AiFillCaretDown className="inline-block" />
+                                <ul
+                                    className={`${
+                                        openGf ? "block" : "hidden"
+                                    } absolute top-20 bg-slate-900 text-xs px-4 w-full [&>*]:[&>*]:h-14 [&>*]:[&>*]:leading-[55px] [&>*]:[&>*]:border-b [&>*]:[&>*]:border-b-blue-400 transition-all ease-linear duration-500`}
+                                >
+                                    <Link to="/guide/whatishe">
+                                        <li className="hover:border-b hover:border-b-yellow-200">
+                                            Juego
+                                        </li>
+                                    </Link>
+                                    <Link to="/guide/terminology">
+                                        <li className="hover:border-b hover:border-b-yellow-200">
+                                            Glosario
+                                        </li>
+                                    </Link>
 
-                        <a
-                            href="https://discord.gg/heroesevolved"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <li className="hover:border-b hover:border-b-yellow-200">
-                                Discord Oficial
+                                    <Link to="/guide/battleground">
+                                        <li className="hover:border-b hover:border-b-yellow-200">
+                                            batalla
+                                        </li>
+                                    </Link>
+
+                                    <Link to="/guide/modes">
+                                        <li className="hover:border-b hover:border-b-yellow-200">
+                                            Modos
+                                        </li>
+                                    </Link>
+
+                                    <Link to="/guide/items">
+                                        <li className="hover:border-b hover:border-b-yellow-200">
+                                            Items
+                                        </li>
+                                    </Link>
+                                    <Link to="/media">
+                                        <li className="hover:border-b hover:border-b-yellow-200">
+                                            Multimedia
+                                        </li>
+                                    </Link>
+                                </ul>
                             </li>
-                        </a>
+                        </Link>
+                        <Link to="/heroes" className="hover:active">
+                            <li className="">Heroes</li>
+                        </Link>
+                        <Link to="/news" className="hover:active">
+                            <li className="">Noticias</li>
+                        </Link>
+                        <Link to="/guide" className="hover:active">
+                            <li className="">Guia</li>
+                        </Link>
+
+                        <div>
+                            <li
+                                className="hover:active"
+                                onMouseEnter={() => {
+                                    setOpenRs(true);
+                                }}
+                                onMouseLeave={() => {
+                                    setOpenRs(false);
+                                }}
+                            >
+                                Comunidad{" "}
+                                <AiFillCaretDown className="inline-block" />
+                                <ul
+                                    className={`${
+                                        openRs ? "block" : "hidden"
+                                    } absolute top-20 bg-slate-900 text-xs px-4 w-full [&>*]:[&>*]:h-14 [&>*]:[&>*]:leading-[55px] [&>*]:[&>*]:border-b [&>*]:[&>*]:border-b-blue-400 transition-all ease-linear duration-500`}
+                                >
+                                    <a
+                                        href="https://www.facebook.com/hem.mawel"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <li className="hover:border-b hover:border-b-yellow-200">
+                                            Facebook
+                                        </li>
+                                    </a>
+                                    <a
+                                        href="https://www.instagram.com/hemmawel/"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <li className="hover:border-b hover:border-b-yellow-200">
+                                            Instagram
+                                        </li>
+                                    </a>
+
+                                    <a
+                                        href="https://discord.gg/heroesevolved"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <li className="hover:border-b hover:border-b-yellow-200">
+                                            Discord Oficial
+                                        </li>
+                                    </a>
+                                </ul>
+                            </li>
+                        </div>
+
+                        <Link to="/download" className="hover:active">
+                            <li className="">Decargas</li>
+                        </Link>
                     </ul>
-                </li>
-                <Link
-                    to="/download"
-                    className="bg-gradient-to-b rounded-r-2xl hover:from-blue-900 hover:to-slate-900 hover:border-b hover:border-b-yellow-200"
-                >
-                    <li className="">Decargas</li>
-                </Link>
-            </ul>
-        </nav>
+                </div>
+            </nav>
+        </div>
     );
 };
