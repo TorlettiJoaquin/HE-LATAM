0938f048a0b6481204d76d558b3a181f6eb99e7b
TorlettiJoaquin <axeltorletti@gmail.com>
2023-07-11 21:37:42 -0300

    add: videos on public folder

--- HELATAM_REACT_VITE_TAILWIND/src/components/Header/Header.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/components/Header/Header.jsx
@@ -1,13 +1,11 @@
-import { TopArticle } from "./TopArticle";
 import { Slogan } from "./Slogan";
 import { VideoBG } from "../../assets/video/hecomprimed.mp4"
 
 export const Header = () => {
     return (
         <header className="-mt-28 w-full mx-auto my-0">
-            <video src={VideoBG}></video>
+            <video src={VideoBG} autoPlay loop muted/>
             <Slogan />
-            <TopArticle />
         </header>
     );
 };
