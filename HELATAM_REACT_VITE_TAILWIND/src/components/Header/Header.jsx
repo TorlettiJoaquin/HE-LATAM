318181f06fde5e5ac2f7657f82e75bef6b9fe06e
TorlettiJoaquin <axeltorletti@gmail.com>
2023-07-11 22:38:00 -0300

    feat: Header with title

--- HELATAM_REACT_VITE_TAILWIND/src/components/Header/Header.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/components/Header/Header.jsx
@@ -1,10 +1,8 @@
-import { Slogan } from "./Slogan";
-
 export const Header = () => {
     return (
-        <header className="-mt-20 w-full h-screen">
-            <video src="/videos/hecomprimed.mp4" autoPlay loop muted className="absolute w-full h-full object-cover"/>
-            <Slogan />
+        <header className="relative -mt-20 w-full h-screen flex">
+            <video src="/videos/hecomprimed2.mp4" autoPlay loop muted className="absolute inset-0 w-full h-full object-cover -z-10" />
+            <h1 className="absolute mr-[30%] pl-60 inset-0 flex items-center justify-center font-bold text-6xl z-10 uppercase tracking-widest leading-normal">Heroes Evolved <br />El primer juego en línea de batalla de héroes 5 vs 5</h1>
         </header>
     );
 };
--- HELATAM_REACT_VITE_TAILWIND/src/components/Header/Slogan.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/components/Header/Slogan.jsx
@@ -1,10 +1,7 @@
-import slogan from "../../assets/images/indextext.webp";
-
 export const Slogan = () => {
     return (
-        <div className="flex justify-center pt-36 pb-72">
-            <h1 className="hidden">Heroes Evolved, the First 5v5 Hero Battle Arena Online Game - From ZERO to HERO</h1>
-            <img src={slogan} alt="The First 5v5 Hero Battle Arena Online Game - From ZERO to HERO"/>
+        <div className="z-10">
+            
         </div>
     );
 };
