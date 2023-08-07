321f41b66ed613f8721035509f6a7f3d9c70947b
TorlettiJoaquin <axeltorletti@gmail.com>
2023-08-07 02:04:40 -0300

    fix: mini fix

--- HELATAM_REACT_VITE_TAILWIND/src/components/Content/MapInteractivo/MapInteractivo.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/components/Content/MapInteractivo/MapInteractivo.jsx
@@ -2,7 +2,7 @@ const MapInteractivo = () => {
   return (
     <div className="h-full pb-10">
       <div className="absolute top-0 w-full h-[100px] bg-gradient-to-b from-black via-[#000000b4] to-transparent"></div>
-      <div className="relative w-full h-[250px] lg:h-[700px] bg-map5v5 bg-no-repeat bg-contain bg-top top-16 "/>
+      <div className="relative w-full h-[250px] lg:h-[700px] bg-map5v5 bg-no-repeat bg-contain bg-top top-16"/>
     </div>
   )
 }
