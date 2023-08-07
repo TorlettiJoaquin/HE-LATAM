dc702ed0fd4cee01f0de9038fea714717afe4aba
TorlettiJoaquin <axeltorletti@gmail.com>
2023-08-07 00:10:06 -0300

    add: video with audio

--- HELATAM_REACT_VITE_TAILWIND/src/components/Header/Header.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export const Header = () => {
         <header className="lg:relative flex lg:-mt-24 w-full h-screen lg:-mb-[90px]">
             <h1 className="flex items-center text-center justify-center absolute lg:w-[70%] pb-48 px-6 mx-auto inset-0 font-semibold font-garamond text-2xl lg:text-[70px] z-10 uppercase tracking-widest lg:leading-relaxed drop-shadow-glow">« Heroes Evolved »<br />El primer juego en línea de batalla de héroes 5 vs 5</h1>
             <News/>
-            <video src="/videos/videoplayback.webm" autoPlay loop muted className="absolute inset-0 w-full h-full object-cover object-center -z-10" />
+            <video src="/videos/hewithaudio.webm" autoPlay loop muted className="absolute inset-0 w-full h-full object-cover object-center -z-10" />
         </header>
     );
 };
