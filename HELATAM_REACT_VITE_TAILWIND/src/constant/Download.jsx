afdf853f647f8f9ff5bb699f13273d35bcad747e
TorlettiJoaquin <axeltorletti@gmail.com>
2023-08-09 03:31:38 -0300

    fix: flex row into dowload icons

--- HELATAM_REACT_VITE_TAILWIND/src/constant/Download.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/constant/Download.jsx
@@ -5,7 +5,7 @@ export const Download = () => {
     return (
         <div className="w-full bg-[#e0bf64] h-44 rounded-t-3xl">
             {/* CORREGIR w-full por w-screen arriba */}
-            <div className="relative w-full h-full flex flex-col justify-center items-center gap-6 lg:gap-10">
+            <div className="relative w-full h-full flex flex-col md:flex-row justify-center items-center gap-6 lg:gap-10">
                 <div className="">
                     <a href="https://apps.apple.com/us/app/heroes-evolved/id1173388413"><img src={AppStore} alt="" className="w-3/4 mx-auto" /></a>
                 </div>
