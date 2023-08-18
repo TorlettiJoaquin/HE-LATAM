43fa624cbd4da2a25c8f69b8a9f28aeae868b5a4
TorlettiJoaquin <axeltorletti@gmail.com>
2023-08-18 17:25:46 -0300

    feat: video map on main content

--- HELATAM_REACT_VITE_TAILWIND/src/components/Content/MapInteractivo/MapInteractivo.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/components/Content/MapInteractivo/MapInteractivo.jsx
@@ -1,8 +1,13 @@
+import mapVideo from '../../../assets/video/mapvideo.webm'
+
 const MapInteractivo = () => {
   return (
-    <div className="h-full pb-10">
+    <div className="h-full pb-10 lg:pb-16">
       <div className="absolute top-0 w-full h-[100px] bg-gradient-to-b from-black via-[#000000b4] to-transparent"></div>
-      <div className="relative w-full h-[250px] lg:h-[700px] bg-map5v5 bg-no-repeat bg-contain bg-top top-16"/>
+      <div className="relative w-full h-[500px] lg:h-[800px] lg:top-16">
+        <video src={mapVideo} autoPlay muted loop className='w-full h-full relative aspect-video object-cover'></video>
+      </div>
+
     </div>
   )
 }
