179a7861475ace0cc82e29fc31114bb29c0bd87a
TorlettiJoaquin <axeltorletti@gmail.com>
2023-08-18 17:26:42 -0300

    feat: stiky qr on download buttons

--- HELATAM_REACT_VITE_TAILWIND/src/constant/Download.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/constant/Download.jsx
@@ -3,9 +3,9 @@ import GooglePlay from "../assets/images/googleplay.webp";
 
 export const Download = () => {
     return (
-        <div className="w-full bg-[#e0bf64] h-44 rounded-t-3xl">
-            {/* CORREGIR w-full por w-screen arriba */}
-            <div className="relative w-full h-full flex flex-col md:flex-row justify-center items-center gap-6 lg:gap-10">
+        <div className="w-full bg-gradient-to-t from-[#e7e6e4] to-[#e7e6e400] bg-opacity-50 h-44 lg:rounded-t-3xl">
+            {/* CORREGIR w-full por w-screen arriba // bg-[#e7e6e4] // bg-[#e0bf64]*/}
+            <div className="relative w-full h-full flex flex-col md:flex-row justify-center items-center gap-3 lg:gap-10 pt-5">
                 <div className="">
                     <a href="https://apps.apple.com/us/app/heroes-evolved/id1173388413"><img src={AppStore} alt="" className="w-3/4 mx-auto" /></a>
                 </div>
