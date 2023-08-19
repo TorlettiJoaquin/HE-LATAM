5659ba5e434cefa68b648fd9a7acdfb986bc1167
TorlettiJoaquin <axeltorletti@gmail.com>
2023-08-18 22:15:41 -0300

    featL space evently at news banner

--- HELATAM_REACT_VITE_TAILWIND/src/components/Content/News/TopArticle.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/components/Content/News/TopArticle.jsx
@@ -4,10 +4,10 @@ export const TopArticle = () => {
     return (
         <div className="lg:absolute w-full lg:w-auto items-center bottom-0 right-0 left-0 z-10 p-6 lg:p-0">
             <ViewMore />
-            <ul className="relative flex flex-col lg:flex-row gap-5 lg:gap-0 justify-center items-center w-full py-5 lg:py-0">
-                <li className="w-[300px] lg:w-[400px] h-[200px] lg:h-[220px] justify-center relative flex flex-wrap hover:-translate-y-3 hover:scale-105 transition-all duration-300">
+            <ul className="relative flex flex-col lg:flex-row gap-5 lg:gap-0 justify-center lg:justify-evenly items-center w-full py-5 lg:py-0">
+                <li className="w-[300px] lg:min-w-[320px] lg:max-w-[400px] h-[200px] lg:h-[220px] justify-center relative flex flex-wrap hover:-translate-y-3 hover:scale-105 transition-all duration-300">
                     <a href="" className="group/img">
-                        <div className="w-full lg:w-[400px] h-full overflow-hidden relative bg-helpcenter bg-top bg-contain bg-no-repeat bg-[#333] drop-shadow-bxshdw text-left border-b-2 border-gray-400 transition-all ease-in-out duration-500 group-hover/img:bg-[rgba(0,0,0,.7) after:bg-[rgba(0,0,0,.7)]">
+                        <div className="w-full lg:min-w-[320px] lg:max-w-[400px] h-full overflow-hidden relative bg-helpcenter bg-top bg-contain bg-no-repeat bg-[#333] drop-shadow-bxshdw text-left border-b-2 border-gray-400 transition-all ease-in-out duration-500 group-hover/img:bg-[rgba(0,0,0,.7) after:bg-[rgba(0,0,0,.7)]">
                             <div className="left-0 bottom-0 right-0 w-full h-full absolute pointer-events-none">
                                 <div className="bg-[linear-gradient(rgba(19,23,28,0)_60%,_rgba(19,23,28,0.733)_70%,_rgb(19,23,28)_90%)] absolute w-full h-full">
                                 </div>
@@ -25,9 +25,9 @@ export const TopArticle = () => {
                         </div>
                     </a>
                 </li>
-                <li className="w-[300px] lg:w-[400px] h-[200px] lg:h-[220px] justify-center relative flex flex-wrap hover:-translate-y-3 hover:scale-105 transition-all duration-300">
+                <li className="w-[300px] lg:min-w-[320px] lg:max-w-[400px] h-[200px] lg:h-[220px] justify-center relative flex flex-wrap hover:-translate-y-3 hover:scale-105 transition-all duration-300">
                     <a href="" className="group/img">
-                        <div className="w-full lg:w-[400px] h-full overflow-hidden relative bg-updateNote bg-top bg-contain bg-no-repeat bg-[#333] drop-shadow-bxshdw text-left border-b-2 border-gray-400 transition-all ease-in-out duration-500 group-hover/img:bg-[rgba(0,0,0,.7) after:bg-[rgba(0,0,0,.7)]">
+                        <div className="w-full lg:min-w-[320px] lg:max-w-[400px] h-full overflow-hidden relative bg-updateNote bg-top bg-contain bg-no-repeat bg-[#333] drop-shadow-bxshdw text-left border-b-2 border-gray-400 transition-all ease-in-out duration-500 group-hover/img:bg-[rgba(0,0,0,.7) after:bg-[rgba(0,0,0,.7)]">
                             <div className="left-0 bottom-0 right-0 w-full h-full absolute pointer-events-none">
                                 <div className="bg-[linear-gradient(rgba(19,23,28,0)_60%,_rgba(19,23,28,0.733)_70%,_rgb(19,23,28)_90%)] absolute w-full h-full">
                                 </div>
@@ -45,9 +45,9 @@ export const TopArticle = () => {
                         </div>
                     </a>
                 </li>
-                <li className="w-[300px] lg:w-[400px] h-[200px] lg:h-[220px] justify-center relative flex flex-wrap hover:-translate-y-3 hover:scale-105 transition-all duration-300">
+                <li className="w-[300px] lg:min-w-[320px] lg:max-w-[400px] h-[200px] lg:h-[220px] justify-center relative flex flex-wrap hover:-translate-y-3 hover:scale-105 transition-all duration-300">
                     <a href="" className="group/img">
-                        <div className="w-full lg:w-[400px] h-full overflow-hidden relative bg-banlist bg-top bg-contain bg-no-repeat bg-[#333] drop-shadow-bxshdw text-left border-b-2 border-gray-400 transition-all ease-in-out duration-500 group-hover/img:bg-[rgba(0,0,0,.7) after:bg-[rgba(0,0,0,.7)]">
+                        <div className="w-full lg:min-w-[320px] lg:max-w-[400px] h-full overflow-hidden relative bg-banlist bg-top bg-contain bg-no-repeat bg-[#333] drop-shadow-bxshdw text-left border-b-2 border-gray-400 transition-all ease-in-out duration-500 group-hover/img:bg-[rgba(0,0,0,.7) after:bg-[rgba(0,0,0,.7)]">
                             <div className="left-0 bottom-0 right-0 w-full h-full absolute pointer-events-none">
                                 <div className="bg-[linear-gradient(rgba(19,23,28,0)_60%,_rgba(19,23,28,0.733)_70%,_rgb(19,23,28)_90%)] absolute w-full h-full">
                                 </div>
--- HELATAM_REACT_VITE_TAILWIND/src/components/Header/ViewMore.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/components/Header/ViewMore.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const ViewMore = () => {
     return (
-        <div className="lg:absolute w-3/6 mx-auto lg:mx-0 place-content-center text-center lg:text-left -top-6 -left-5 flex flex-col lg:flex-row lg:gap-10 z-10">
+        <div className="lg:absolute w-3/6 mx-auto lg:mx-0 place-content-center text-center lg:text-left -top-6 lg:-top-10 -left-5 flex flex-col lg:flex-row lg:gap-10 z-10">
             <div className="font-bold text-gray-400 z-10">ULTIMAS NOTICIAS</div>
             <Link
                 to="/news"
