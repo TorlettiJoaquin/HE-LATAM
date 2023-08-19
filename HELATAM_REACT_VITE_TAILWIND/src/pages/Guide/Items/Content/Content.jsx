cdb700de1afecc13666ca37ccdee32cfd0ede853
TorlettiJoaquin <axeltorletti@gmail.com>
2023-08-19 19:34:43 -0300

    feat: item without modal ready

--- HELATAM_REACT_VITE_TAILWIND/src/pages/Guide/Items/Content/Content.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/pages/Guide/Items/Content/Content.jsx
@@ -3,13 +3,13 @@ import ItemData2 from "./ItemData2";
 
 export const Content = () => {
     return (
-        <section className="max-w-screen-lg w-full mx-auto my-0 relative pb-[180px] ">
-            <div className="">
-                <section className="text-[#adceff] text-base py-10">
-                    <h1 className="text-3xl xl:text-4xl font-extrabold tracking-widest bg-gradient-to-b from-orange-500 to-yellow-500 text-transparent bg-clip-text drop-shadow-glow font-garamond px-5 py-3 text-center">
+        <section className="max-w-screen-lg w-full mx-auto my-0 relative">
+            <div className="px-10 md:px-5">
+                <section className="text-[#adceff] text-base pb-5 md:py-10">
+                    <h1 className="text-2xl xl:text-4xl font-extrabold tracking-widest bg-gradient-to-b from-orange-500 to-yellow-500 text-transparent bg-clip-text drop-shadow-glow font-garamond px-5 py-3 text-center">
                         Items y Equipamiento
                     </h1>
-                    <p className="py-5 text-xl font-garamond font-bold text-center">
+                    <p className="py-5 text-lg xl:text-xl font-garamond font-bold text-center">
                         Mientras el juego progresa irás adquiriendo oro que
                         puedes gastar en ítems más poderosos que mejoran las
                         características de tu héroe. Estos ítems pueden varios
@@ -20,41 +20,39 @@ export const Content = () => {
                         donde apareces al comienzo del juego.
                     </p>
                 </section>
-                <ul className="flex justify-around text-center font-semibold bg-indigo-950 text-xl border-x [&>*]:uppercase [&>*]:cursor-pointer font-garamond">
-                    <li className="w-1/6 leading-10 text-center hover:bg-indigo-900">
+                <ul className="flex flex-col md:flex-row justify-around text-center font-semibold bg-indigo-950 text-xl [&>*]md:w-1/6 [&>*]:w-full border-y [&>*]:border-y [&>*]:md:border-x [&>*]:uppercase [&>*]:cursor-pointer font-garamond">
+                    <li className=" leading-10 text-center hover:bg-indigo-900">
                         Todos
                     </li>
-                    <li className="w-1/6 leading-10 text-center hover:bg-indigo-900">
+                    <li className="leading-10 text-center hover:bg-indigo-900">
                         Fisicos
                     </li>
-                    <li className="w-1/6 leading-10 text-center hover:bg-indigo-900">
+                    <li className="leading-10 text-center hover:bg-indigo-900">
                         Magicos
                     </li>
-                    <li className="w-1/6 leading-10 text-center hover:bg-indigo-900">
+                    <li className="leading-10 text-center hover:bg-indigo-900">
                         Defensivos
                     </li>
-                    <li className="w-1/6 leading-10 text-center hover:bg-indigo-900">
-                        Tillas
+                    <li className="leading-10 text-center hover:bg-indigo-900">
+                        Botas
                     </li>
-                    <li className="w-1/6 leading-10 text-center hover:bg-indigo-900">
+                    <li className="leading-10 text-center hover:bg-indigo-900">
                         Vigilante
                     </li>
                 </ul>
-                <div>
-                    <ul className="p-[30px] mx-auto my-0">
-                        {ItemData2.map((item, idx) => (
-                            <ItemCard
-                                key={idx}
-                                effect={item.effect}
-                                img={item.img}
-                                lvl={item.lvl}
-                                name={item.name}
-                                prize={item.prize}
-                                prop={item.prop}
-                            />
-                        ))}
-                    </ul>
-                </div>
+                <ul className="grid grid-cols-[repeat(auto-fill,_minmax(100px,_1fr))] 2xl:grid-cols-[repeat(auto-fill,_minmax(125px,_1fr))] gap-4 p-4">
+                    {ItemData2.map((item, idx) => (
+                        <ItemCard
+                            key={idx}
+                            effect={item.effect}
+                            img={item.img}
+                            lvl={item.lvl}
+                            name={item.name}
+                            prize={item.prize}
+                            prop={item.prop}
+                        />
+                    ))}
+                </ul>
             </div>
         </section>
     );
--- HELATAM_REACT_VITE_TAILWIND/src/pages/Guide/Items/Content/ItemCard.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/pages/Guide/Items/Content/ItemCard.jsx
@@ -1,68 +1,56 @@
 import { useState } from "react";
 
-import PropTypes from "prop-types";
-
 export const ItemCard = ({ name, img, prop, effect, prize, lvl }) => {
     const [showItem, setShowItem] = useState(false);
 
     const iPrefix = "https://img6.99.com/yhkd/image/data/equip/";
 
     return (
-        <div>
-            <li
-                onMouseEnter={() => {
-                    setShowItem(true);
-                }}
-                onMouseLeave={() => {
-                    setShowItem(false);
-                }}
-                className="relative float-left p-1 mx-[18px] my-[10px] border-4 border-purple-900 rounded-2xl"
-            >
-                <img
-                    src={`${iPrefix}${img}`}
-                    alt={`${name} imagen`} 
-                    className="w-[100px] h-[100px] rounded-lg"
-                />
-                <div
-                    className={`${
-                        showItem ? "block" : "hidden"
+
+        <li
+            onMouseEnter={() => {
+                setShowItem(true);
+            }}
+            onMouseLeave={() => {
+                setShowItem(false);
+            }}
+            className="relative p-1 m-[8px] border-4 border-purple-900 rounded-2xl aspect-square"
+        >
+            <img
+                src={`${iPrefix}${img}`}
+                alt={`${name}`}
+                className="aspect-square w-full h-full rounded-lg"
+            />
+            <div
+                className={`${showItem ? "block" : "hidden"
                     } absolute top-[60px] left-[60px] w-[620px] p-[20px] bg-black border border-[#0f3db8] z-50 rounded-xl`}
-                >
-                    <p className="py-[5px]">
-                        <img
-                            src={`${iPrefix}${img}`}
-                            alt={`${name} imagen`} 
-                            className="w-[64px] h-[64px] mr-[20px] float-left border border-[#0f3db8]"
-                        />
-                        <span className="text-[#adceff]">
-                            {name}
-                        </span>
-                        <br />
-                        <span className="text-[#adceff]">Precio: {prize}</span>
-                        <br />
-                        <span className="text-[#adceff]">Star Level: {lvl}</span>
-                    </p>
-                    <p>
-                        Attribute: {"  "}
-                        <span className="text-[#adceff]">{prop}</span>
-                    </p>
+            >
+                <p className="py-[5px]">
+                    <img
+                        src={`${iPrefix}${img}`}
+                        alt={`${name}`}
+                        className="w-[64px] h-[64px] mr-[20px] float-left border border-[#0f3db8]"
+                    />
+                    <span className="text-[#adceff]">
+                        {name}
+                    </span>
                     <br />
-                    <p>
-                        <span className="text-[#adceff]">
-                            {effect}
-                        </span>
-                    </p>
-                </div>
-            </li>
-        </div>
+                    <span className="text-[#adceff]">Precio: {prize}</span>
+                    <br />
+                    <span className="text-[#adceff]">Star Level: {lvl}</span>
+                </p>
+                <p>
+                    Attribute: {"  "}
+                    <span className="text-[#adceff]">{prop}</span>
+                </p>
+                <br />
+                <p>
+                    <span className="text-[#adceff]">
+                        {effect}
+                    </span>
+                </p>
+            </div>
+        </li>
+
     );
 };
-
-ItemCard.propTypes = {
-    name: PropTypes.string.isRequired,
-    img: PropTypes.string.isRequired,
-    prop: PropTypes.array.isRequired,
-    effect: PropTypes.array.isRequired,
-    prize: PropTypes.number.isRequired,
-    lvl: PropTypes.number.isRequired,
-}
