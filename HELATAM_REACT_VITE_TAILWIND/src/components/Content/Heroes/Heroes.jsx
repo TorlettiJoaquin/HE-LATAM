31e2d36b087fe91abc210439f5728274441e858f
TorlettiJoaquin <axeltorletti@gmail.com>
2023-08-18 22:00:31 -0300

    feat: heroes title

--- HELATAM_REACT_VITE_TAILWIND/src/components/Content/Heroes/Heroes.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/components/Content/Heroes/Heroes.jsx
@@ -24,17 +24,23 @@ const InfiniteLoopSlider = ({ children, duration, reverse = false }) => {
 const Heroes = () => {
     return (
         <div className='relative'>
-            <div className='pb-10 md:py-10 w-[80%] mx-auto min-h-0 flex flex-col relative overflow-hidden gap-4'>
-                {[...new Array(ROWS)].map((_, i) => (
-                    <InfiniteLoopSlider key={i} duration={random(DURATION - 5, DURATION + 5)} reverse={i % 2}>
-                        {shuffle(data.HeroesData).slice(0, TAGS_PER_ROW).map(hero => (
-                            <img src={`https://img6.99.com/yhkd/image/data/hero/head/${hero.id}.jpg`} key={hero.id} className='object-contain flex items-center gap-[0 0.2rem] h-28 md:h-36 rounded-xl mr-4 shadow-[0_0.1rem_0.2rem_#000]' />
-                        ))}
-                    </InfiniteLoopSlider>
-                ))}
-                {/* <div className='fade' /> */}
+            <div className='flex justify-center pb-5 text-lg'>
+                <h1 className='text-3xl xl:text-4xl font-extrabold tracking-widest bg-gradient-to-b from-orange-500 to-yellow-500 text-transparent bg-clip-text drop-shadow-glow font-garamond px-5 py-3'>Elige tus HEROES INMORTALES</h1>
             </div>
-            <div className="absolute right-0 top-0 left-0 bottom-0 h-full w-[85%] mx-auto bg-gradient-to-r from-black via-transparent to-black" />
+            <div className='relative'>
+                <div className='pb-10 md:py-10 w-[80%] mx-auto min-h-0 flex flex-col relative overflow-hidden gap-4'>
+                    {[...new Array(ROWS)].map((_, i) => (
+                        <InfiniteLoopSlider key={i} duration={random(DURATION - 5, DURATION + 5)} reverse={i % 2}>
+                            {shuffle(data.HeroesData).slice(0, TAGS_PER_ROW).map(hero => (
+                                <img src={`https://img6.99.com/yhkd/image/data/hero/head/${hero.id}.jpg`} key={hero.id} className='object-contain flex items-center gap-[0 0.2rem] h-28 md:h-36 rounded-xl mr-4 shadow-[0_0.1rem_0.2rem_#000]' />
+                            ))}
+                        </InfiniteLoopSlider>
+                    ))}
+                    {/* <div className='fade' /> */}
+                </div>
+                <div className="absolute right-0 top-0 left-0 bottom-0 h-full w-[85%] mx-auto bg-gradient-to-r from-black via-transparent to-black" />
+            </div>
+
 
         </div>
 
--- HELATAM_REACT_VITE_TAILWIND/src/components/Content/MapInteractivo/MapInteractivo.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/components/Content/MapInteractivo/MapInteractivo.jsx
@@ -2,12 +2,12 @@ import mapVideo from '../../../assets/video/mapvideo.webm'
 
 const MapInteractivo = () => {
   return (
-    <div className="h-full pb-10 lg:pb-16">
-      <div className="absolute top-0 w-full h-[100px] bg-gradient-to-b from-black via-[#000000b4] to-transparent"></div>
+    <div className="h-full pb-10 lg:pb-20 relative">
       <div className="relative w-full h-[500px] lg:h-[800px] lg:top-16">
         <video src={mapVideo} autoPlay muted loop className='w-full h-full relative aspect-video object-cover'></video>
+        <h1 className='absolute z-10 inset-x-0 top-1/4 text-center text-4xl xl:text-5xl font-extrabold tracking-widest bg-gradient-to-b from-orange-500 to-yellow-500 text-transparent bg-clip-text drop-shadow-glow font-garamond px-5'>Ingresa al <br /> CAMPO DE BATALLA</h1>
       </div>
-
+      <div className="absolute right-0 top-0 left-0 bottom-0 h-full mx-auto bg-gradient-to-b from-black via-transparent to-black" />
     </div>
   )
 }
