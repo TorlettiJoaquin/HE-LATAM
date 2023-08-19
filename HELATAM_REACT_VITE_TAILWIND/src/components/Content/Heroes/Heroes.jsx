b25f5357e1974a148d5909fcebaac17810588b06
TorlettiJoaquin <axeltorletti@gmail.com>
2023-08-18 21:34:50 -0300

    Perfect heroes card

--- HELATAM_REACT_VITE_TAILWIND/src/components/Content/Heroes/Heroes.jsx
+++ HELATAM_REACT_VITE_TAILWIND/src/components/Content/Heroes/Heroes.jsx
@@ -1,8 +1,8 @@
 import data from '../../../constant/Heroes/HeroesData'
 
-const DURATION = 15000;
+const DURATION = 120;
 const ROWS = 5;
-const TAGS_PER_ROW = 5;
+const TAGS_PER_ROW = 130;
 
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 const shuffle = (arr) => [...arr].sort(() => .5 - Math.random());
@@ -10,10 +10,10 @@ const shuffle = (arr) => [...arr].sort(() => .5 - Math.random());
 const InfiniteLoopSlider = ({ children, duration, reverse = false }) => {
     return (
         <div className='loop-slider' style={{
-            '--duration': `${duration}ms`,
+            '--duration': `${duration}s`,
             '--direction': reverse ? 'reverse' : 'normal'
         }}>
-            <div className='inner'>
+            <div className='inner flex w-fit'>
                 {children}
                 {children}
             </div>
@@ -23,20 +23,21 @@ const InfiniteLoopSlider = ({ children, duration, reverse = false }) => {
 
 const Heroes = () => {
     return (
-        <div className='heroes'>
-
-            <div className='tag-list'>
-
+        <div className='relative'>
+            <div className='pb-10 md:py-10 w-[80%] mx-auto min-h-0 flex flex-col relative overflow-hidden gap-4'>
                 {[...new Array(ROWS)].map((_, i) => (
-                    <InfiniteLoopSlider key={i} duration={random(DURATION - 5000, DURATION + 5000)} reverse={i % 2}>
+                    <InfiniteLoopSlider key={i} duration={random(DURATION - 5, DURATION + 5)} reverse={i % 2}>
                         {shuffle(data.HeroesData).slice(0, TAGS_PER_ROW).map(hero => (
-                            <img src={`https://img6.99.com/yhkd/image/data/hero/head/${hero.id}.jpg`} key={hero.id} className='object-contain tag'/>
+                            <img src={`https://img6.99.com/yhkd/image/data/hero/head/${hero.id}.jpg`} key={hero.id} className='object-contain flex items-center gap-[0 0.2rem] h-28 md:h-36 rounded-xl mr-4 shadow-[0_0.1rem_0.2rem_#000]' />
                         ))}
                     </InfiniteLoopSlider>
                 ))}
-                <div className='fade' />
+                {/* <div className='fade' /> */}
             </div>
+            <div className="absolute right-0 top-0 left-0 bottom-0 h-full w-[85%] mx-auto bg-gradient-to-r from-black via-transparent to-black" />
+
         </div>
+
     )
 }
 
