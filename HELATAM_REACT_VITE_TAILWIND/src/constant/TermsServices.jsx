3d40dd45bed972214ef67922575cce28f68ef354
TorlettiJoaquin <axeltorletti@gmail.com>
2023-08-19 17:03:22 -0300

    feat: term of serv sect

---
+++ HELATAM_REACT_VITE_TAILWIND/src/constant/TermsServices.jsx
@@ -0,0 +1,37 @@
+import { Footer } from "./Footer"
+import { Navbar } from "./Navbar"
+
+export const TermsServices = () => {
+    return (
+        <section className="bg-newbg-bg bg-fixed bg-cover">
+            <Navbar />
+            <div className="flex justify-center">
+                {/* Dos opciones, backdrop-blur-3xl para fondo blur o bg-[#0f2b55] para fondo lizo */}
+                <div className="max-w-screen-lg p-10 rounded-xl backdrop-blur-3xl m-5">
+                    <h1 className="text-3xl font-bold text-white">Terminos de servicio</h1>
+                    <p className="text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam quam alias sed sunt est eos distinctio corrupti tempora ea tenetur! Quaerat recusandae asperiores vitae, fuga explicabo dolor dolore ipsum culpa!
+                        Unde est soluta iure hic molestias recusandae odio, quasi facere. Facere ipsum molestiae eveniet ipsa officia consectetur cum ab eum eaque praesentium quia veritatis minima voluptate doloribus, in itaque quas.
+                        Ex earum quas consequatur dolorem quam quisquam? Itaque deserunt ipsam repellat, fugiat sit optio rerum odio beatae ex molestias a dignissimos. Necessitatibus, earum libero magnam ad quam in labore eaque.
+                        Accusamus dolore itaque cum aliquam et eveniet reiciendis rem? Quis, consequuntur quam error omnis mollitia et facilis iusto ea, autem a voluptates aspernatur rem, obcaecati corrupti inventore! Voluptate, omnis odio.
+                        Consequuntur magni aliquid impedit mollitia rerum facere a. Quidem aspernatur esse a labore repellat quas dolorum qui molestiae, hic aperiam, rerum molestias consequatur fuga doloribus vero? Iste sapiente placeat officia.
+                        Quos error dignissimos, inventore sapiente illum ipsum voluptatem ullam consectetur, nulla non ex. Itaque quidem repellat adipisci, quo assumenda recusandae necessitatibus sed odio, quas veniam corrupti eveniet ducimus commodi. Quibusdam!
+                        Libero, quas. Mollitia tenetur unde, quaerat et, reprehenderit aut maiores, in eius cumque accusantium dicta voluptatibus. Natus dolor quos maxime id? Ipsa earum eligendi nostrum sed ducimus, nam dolores optio.
+                        Quidem ducimus ipsa quia, excepturi iure doloribus porro nam, esse quaerat, sapiente eius. Distinctio dolore quidem eius voluptates quasi dolorem, maiores quaerat libero sapiente, quos ullam rerum sunt aperiam harum.
+                        Enim pariatur qui similique, quasi quae in exercitationem officia hic voluptas tempore dicta iste possimus, veritatis culpa. Voluptatem, impedit? Voluptas voluptatum magnam assumenda vel veritatis laudantium aliquam ea inventore ullam?
+                        Enim corporis beatae unde illum pariatur. Enim, adipisci a. Veritatis labore sequi dolor nam maxime velit a, beatae voluptatum cum officia provident, incidunt nulla laboriosam! Molestiae minima quas commodi. Repellat?
+                        Iusto dicta animi culpa quis deleniti, id ad perspiciatis hic rem esse quidem tenetur excepturi libero officia! Voluptatibus aliquam magni, sunt molestiae illum autem at odit impedit dicta, et expedita?
+                        Reprehenderit architecto deleniti, dolore ullam dicta omnis accusamus sapiente consequatur sequi ratione quae fugit minima nisi libero consequuntur, optio, eos possimus assumenda rem ea repellat! Nihil hic dolor maiores nobis.
+                        Nemo temporibus alias, eum explicabo recusandae provident adipisci voluptatem culpa rem, qui beatae autem nesciunt! Ut ab magni quidem! Maxime, veniam! Dicta reiciendis numquam obcaecati veritatis ad nam magni provident?
+                        Quos porro cumque eligendi quod illo tempora soluta quidem nisi accusantium velit odit atque repellat alias quasi numquam exercitationem corrupti, provident, dolore, veritatis aperiam quae a adipisci pariatur? Atque, veritatis.
+                        Adipisci distinctio numquam libero atque illum hic molestiae officiis minus quis, a unde sequi perspiciatis quia modi. Perspiciatis at, ipsum pariatur, vero tempora doloremque a, quis dolore voluptas nam temporibus!
+                        A voluptas rem esse nobis, dicta dolores autem labore laboriosam incidunt porro earum veniam, optio magni laudantium voluptates repellendus obcaecati sint nihil aperiam ipsam eaque inventore reprehenderit. Mollitia, nesciunt veniam!
+                        Reiciendis tenetur repellendus ratione quia explicabo provident exercitationem magni numquam. Quam excepturi eaque cum officia molestias voluptatum debitis! Tempora odit facilis itaque veniam aut sapiente nam alias doloribus omnis beatae?
+                        Nihil labore facere eius atque molestiae repudiandae, ipsa enim possimus, velit impedit laudantium, consequuntur quis doloribus voluptatum. At fuga labore voluptatibus inventore facilis, aut cum consequatur explicabo deserunt ex itaque.
+                        Quia mollitia, accusamus et obcaecati alias atque quaerat, est reiciendis distinctio doloremque earum laborum. Debitis explicabo facere distinctio esse perferendis nemo. Saepe illum molestias hic exercitationem dolorum repellendus harum libero.
+                        Ducimus, magni ipsam necessitatibus error iste aliquid maxime natus nesciunt tempora expedita totam. Autem blanditiis at ipsa natus sit hic fugiat earum doloribus incidunt sequi, soluta accusamus illo ipsum quod.</p>
+                </div>
+            </div>
+            <Footer />
+        </section>
+    )
+}
